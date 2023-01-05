6d29a2e587c1b5f2e21a7f08065eb2651960d0c0
Prasanna <devi10.alapati@gmail.com>
2023-01-05 12:32:15 +0530

    tf-jen files

---
+++ main.tf
@@ -0,0 +1,9 @@
+source  = "hashicorp/aws"
+version = "4.48.0"
+      provider "aws" {
+  # Configuration options
+  region = "ap-south-1"
+access_key = "AKIA3LLE2F6BPGP65MOT"
+secret_key = "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"
+  }
+
