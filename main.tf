a336c1ab0f435bda8e95705f4b7ecebd5ff46862
prasannaburugapalli <103118469+prasannaburugapalli@users.noreply.github.com>
2023-01-05 15:17:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
-source  = "hashicorp/aws"
-version = "4.48.0"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.20.1"
+    }
+  }
+}
       provider "aws" {
   # Configuration options
   region = "ap-south-1"
 access_key = "AKIA3LLE2F6BPGP65MOT"
 secret_key = "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"
   }
+
 
